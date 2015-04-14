8634f002ac27518ca60047b03b78c8202c6fd95e
Toby Dylan Hocking <tdhock5@gmail.com>
2015-04-14 16:14:40 -0400

    PeakSeg/Joint comparison fig + timings tables

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-HOCKING-PeakSegJoint-slides.pdf: HOCKING-PeakSegJoint-slides.tex figure-profiles.tex
+HOCKING-PeakSegJoint-slides.pdf: HOCKING-PeakSegJoint-slides.tex figure-profiles.tex table-timings.tex
 	rm -f *.aux *.bbl
 	pdflatex HOCKING-PeakSegJoint-slides
 figure-profiles.tex: figure-profiles.R
+	R --no-save < $<
+table-timings.tex: table-timings.R
 	R --no-save < $<
---
+++ table-timings.R
@@ -0,0 +1,337 @@
+works_with_R("3.1.3",
+             xtable="1.7.4",
+             data.table="1.9.5",
+             ggplot2="1.0",
+             dplyr="0.4.0",
+             "tdhock/PeakSegDP@fe06a5b91d68c5d1ec471cb15c3ec3935dc2624d",
+             "tdhock/PeakSegJoint@547ca81ce92c38b7c7f78cd071efc5afa96cb289",
+             "tdhock/PeakError@d9196abd9ba51ad1b8f165d49870039593b94732")
+
+data(H3K36me3.TDH.other.chunk1)
+
+ann.colors <-
+  c(noPeaks="#f6f4bf",
+    peakStart="#ffafaf",
+    peakEnd="#ff4c4c",
+    peaks="#a445ee")
+
+profile.list <- with(H3K36me3.TDH.other.chunk1, split(counts, counts$sample.id))
+region.list <-
+  with(H3K36me3.TDH.other.chunk1, split(regions, regions$sample.id))
+chrom.range <- with(H3K36me3.TDH.other.chunk1$counts, {
+  c(min(chromStart), max(chromEnd))
+})
+chrom.bases <- chrom.range[2]-chrom.range[1]
+n.bins <- 2000L
+bases.per.bin <- as.integer(chrom.bases/n.bins)
+fit.list <- list()
+timing.list <- list()
+error.region.list <- list()
+peak.list <- list()
+for(sample.id in names(profile.list)){
+  profile <- profile.list[[sample.id]]
+  regions <- region.list[[sample.id]]
+  seconds <- system.time({
+    fit.list[[sample.id]] <- fit <- 
+      segmentBins(profile,
+                  chrom.range[1], bases.per.bin)
+  })[["elapsed"]]
+  sample.error.list <- list()
+  sample.region.list <- list()
+  for(peaks.str in names(fit$fit$peaks)){
+    peaks.df <- fit$fit$peaks[[peaks.str]]
+    peaks <- as.numeric(peaks.str)
+    error <- PeakErrorChrom(peaks.df, regions)
+    sample.region.list[[peaks.str]] <-
+      data.frame(sample.id, peaks, error)
+    sample.error.list[[peaks.str]] <-
+      data.frame(sample.id, peaks,
+                 errors=with(error, sum(fp+fn)))
+  }
+  errors <- do.call(rbind, sample.error.list)
+  best.error <- errors[which.min(errors$errors),]
+  peaks.str <- paste(best.error$peaks)
+  max.count <- max(profile$count)
+  h <- max.count/10
+  y <- -h
+  error.region.list[[sample.id]] <-
+    data.frame(ymin=y-h, ymax=y+h, sample.region.list[[peaks.str]])
+  peak.list[[sample.id]] <-
+    data.frame(sample.id, fit$fit$peaks[[peaks.str]], y)
+  timing.list[[sample.id]] <- data.frame(seconds, sample.id)
+}
+error.regions <- do.call(rbind, error.region.list)
+peaks.df <- do.call(rbind, peak.list)
+
+bases.per.problem.vec <- (4.5) * (2^(12:17))
+
+count.dt <- data.table(H3K36me3.TDH.other.chunk1$counts)
+setkey(count.dt, chromStart, chromEnd)
+
+regions.dt <- data.table(H3K36me3.TDH.other.chunk1$regions)
+regions.dt$region.i <- 1:nrow(regions.dt)
+setkey(regions.dt, chromStart, chromEnd)
+
+joint.time.list <- list()
+best.peak.list <- list()
+resolution.err.list <- list()
+problem.list <- list()
+for(bases.per.problem in bases.per.problem.vec){
+  problemStart <- seq(0, chrom.range[2], by=bases.per.problem)
+  problemStart <- as.integer(c(problemStart, problemStart+bases.per.problem/2))
+  problemEnd <- problemStart+bases.per.problem
+  peakStart <- as.integer(problemStart+bases.per.problem/4)
+  peakEnd <- as.integer(problemEnd-bases.per.problem/4)
+  is.overlap <- chrom.range[1] < problemEnd
+  problem.name <- sprintf("chr21:%d-%d", problemStart, problemEnd)
+  problems <- 
+    data.frame(problem.name, peakStart, peakEnd,
+               bases.per.problem, problemStart, problemEnd)[is.overlap,]
+  problems.dt <- data.table(problems)
+  setkey(problems.dt, problemStart, problemEnd)
+  problems.dt$problem.i <- 1:nrow(problems.dt)
+
+  ggplot()+
+    geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                      fill=annotation),
+                  alpha=0.5,
+                  color="grey",
+                  data=H3K36me3.TDH.other.chunk1$regions)+
+    scale_fill_manual(values=ann.colors)+
+    geom_segment(aes(problemStart/1e3, problem.name,
+                     xend=problemEnd/1e3, yend=problem.name),
+                 data=problems)
+
+  overlap.regions <- foverlaps(regions.dt, problems.dt, nomatch=0L)
+  region.counts <- table(overlap.regions$region.i)
+  region.weights <- 1/region.counts
+  overlap.regions[, weight := region.weights[paste(region.i)]]
+  table(paste(overlap.regions$problem.name))
+
+  problems.with.regions <- paste(unique(overlap.regions$problem.name))
+  setkey(overlap.regions, problem.name)
+  setkey(problems.dt, problem.name)
+  problem.list[[paste(bases.per.problem)]] <-
+    problems.dt[problems.with.regions]
+  resolution.stat.list <- list()
+  for(problem.name in problems.with.regions){
+    problem.dt <- problems.dt[problem.name]
+    setkey(problem.dt, problemStart, problemEnd)
+    problem.counts <- foverlaps(count.dt, problem.dt, nomatch=0L)
+    problem.regions <- overlap.regions[problem.name]
+    regions.by.sample <- split(problem.regions, problem.regions$sample.id)
+
+    seconds <- system.time({
+      fit <- PeakSegJointHeuristic(problem.counts)
+      converted <- ConvertModelList(fit)
+    })[["elapsed"]]
+
+    joint.time.list[[problem.name]] <-
+      data.frame(problem.dt, seconds,
+                 data=nrow(problem.counts))
+    
+    peaks.by.peaks <- if(!is.null(converted$peaks$peaks)){
+      with(converted, split(peaks, peaks$peaks))
+    }else{
+      list()
+    }
+    peaks.by.peaks[["0"]] <- Peaks()
+    problem.err.list <- list()
+    problem.stats.list <- list()
+    for(peaks.str in names(peaks.by.peaks)){
+      peaks.all.samples <- peaks.by.peaks[[peaks.str]]
+      peaks.by.sample <-
+        split(peaks.all.samples, peaks.all.samples$sample.id, drop=TRUE)
+      peaks.stats.list <- list()
+      for(sample.id in names(regions.by.sample)){
+        regions <- regions.by.sample[[sample.id]]
+        peaks <- if(sample.id %in% names(peaks.by.sample)){
+          peaks.by.sample[[sample.id]]
+        }else{
+          Peaks()
+        }
+        error <- PeakErrorChrom(peaks, regions)
+        error$weight <- regions$weight
+        error$errors <- with(error, fp+fn)
+        error$weighted.error <- with(error, errors * weight)
+        problem.err.list[[peaks.str]][[sample.id]] <-
+          data.frame(error, sample.id)
+        peaks.stats.list[[sample.id]] <-
+          data.frame(sample.id, weighted.error=sum(error$weighted.error),
+                     total.weight=sum(error$weight))
+      }
+      peaks.stats <- do.call(rbind, peaks.stats.list)
+      problem.stats.list[[peaks.str]] <-
+        data.frame(peaks=as.numeric(peaks.str),
+                   weighted.error=sum(peaks.stats$weighted.error),
+                   total.weight=sum(peaks.stats$total.weight))
+    }
+    problem.stats <- do.call(rbind, problem.stats.list)
+    problem.stats <- problem.stats[order(problem.stats$peaks), ]
+    best.error <- problem.stats[which.min(problem.stats$weighted.error),]
+    if(best.error$weighted.error > 0){
+      ##stop("non-zero error")
+    }
+    resolution.stat.list[[problem.name]] <- best.error
+
+    ggplot(,aes(peaks, weighted.error))+
+      geom_line(data=problem.stats)+
+      geom_point(data=best.error)
+    
+    peaks.str <- paste(best.error$peaks)
+    best.regions <- do.call(rbind, problem.err.list[[peaks.str]])
+    best.peaks <- peaks.by.peaks[[peaks.str]]
+
+    if(nrow(best.peaks)){
+      best.peak.list[[paste(bases.per.problem)]][[problem.name]] <-
+        data.frame(best.peaks, problem.i=problem.dt$problem.i)
+    }
+
+    ggplot()+
+      scale_linetype_manual("error type",
+                            limits=c("correct", 
+                              "false negative",
+                              "false positive"
+                                     ),
+                            values=c(correct=0,
+                              "false negative"=3,
+                              "false positive"=1))+
+      xlab("position on chromosome (kilo bases = kb)")+
+      geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                        fill=annotation),
+                    alpha=0.5,
+                    color="grey",
+                    data=best.regions)+
+      geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                        linetype=status),
+                    fill=NA,
+                    size=2,
+                    color="black",
+                    data=best.regions)+
+      scale_y_continuous("aligned read coverage",
+                         breaks=function(limits){
+                           floor(limits[2])
+                         })+
+      xlab("position on chromosome (kilo bases = kb)")+
+      geom_line(aes(chromStart/1e3, count),
+                data=problem.counts,
+                color="grey50")+
+      geom_segment(aes(chromStart/1e3, 0,
+                       xend=chromEnd/1e3, yend=0),
+                   size=3,
+                   color="deepskyblue",
+                   data=best.peaks)+
+      theme_bw()+
+      scale_fill_manual(values=ann.colors)+
+      theme(panel.margin=grid::unit(0, "cm"))+
+      facet_grid(sample.id ~ ., scales="free", labeller=function(var, val){
+        sub("McGill0", "", val)
+      })
+  }#problem.name
+  resolution.stats <- do.call(rbind, resolution.stat.list)
+  stopifnot(all.equal(sum(resolution.stats$total.weight), nrow(regions.dt)))
+  resolution.err.list[[paste(bases.per.problem)]] <-
+    data.frame(bases.per.problem,
+               weighted.error=sum(resolution.stats$weighted.error))
+}
+resolution.err <- do.call(rbind, resolution.err.list)
+best.res <- resolution.err[which.min(resolution.err$weighted.error), ]
+best.res.str <- paste(best.res$bases.per.problem)
+best.peaks <- do.call(rbind, best.peak.list[[best.res.str]])
+best.problems <- problem.list[[best.res.str]]
+prob.peaks <- best.peaks
+prob.peaks$sample.id <- "problems"
+
+all.problems <- do.call(rbind, problem.list)
+ggplot()+
+  geom_point(aes(problemStart/1e3, bases.per.problem),
+               data=all.problems)+
+  geom_segment(aes(problemStart/1e3, bases.per.problem,
+                   xend=problemEnd/1e3, yend=bases.per.problem),
+               data=all.problems)+
+  scale_y_log10()
+
+problemsPlot <- 
+ggplot()+
+  scale_y_continuous("aligned read coverage",
+                     breaks=function(limits){
+                       floor(limits[2])
+                     })+
+  scale_linetype_manual("error type",
+                        limits=c("correct", 
+                          "false negative",
+                          "false positive"
+                                 ),
+                        values=c(correct=0,
+                          "false negative"=3,
+                          "false positive"=1))+
+  geom_blank(aes(linetype=status),
+             data=data.frame(status=c("correct", "false positive",
+                               "false negative")))+
+  xlab("position on chromosome (kilo bases = kb)")+
+  geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                    fill=annotation),
+                alpha=0.5,
+                color="grey",
+                data=H3K36me3.TDH.other.chunk1$regions)+
+  scale_color_manual(values=c(PeakSeg="deepskyblue",
+                       PeakSegJoint="black"),
+                     limits=c("PeakSeg", "PeakSegJoint"))+
+  geom_segment(aes(chromStart/1e3, 0,
+                   color=model,
+                   xend=chromEnd/1e3, yend=0),
+               size=2,
+               data=data.frame(peaks.df, model="PeakSeg"))+
+  ## geom_segment(aes(chromStart/1e3, 0,
+  ##                  color=model,
+  ##                  xend=chromEnd/1e3, yend=0),
+  ##              size=2,
+  ##              data=data.frame(best.peaks, model="PeakSegJoint"))+
+  scale_fill_manual(values=ann.colors)+
+  geom_step(aes(chromStart/1e3, count),
+            data=H3K36me3.TDH.other.chunk1$counts,
+            color="grey50")+
+  theme_bw()+
+  geom_segment(aes(chromStart/1e3, problem.i,
+                   color=model,
+                   xend=chromEnd/1e3, yend=problem.i),
+               size=2,
+               data=data.frame(prob.peaks, model="PeakSegJoint"))+
+  geom_segment(aes(problemStart/1e3, problem.i,
+                   xend=problemEnd/1e3, yend=problem.i),
+               data=data.frame(sample.id="problems", best.problems))+
+  theme(panel.margin=grid::unit(0, "cm"))+
+  coord_cartesian(xlim=with(H3K36me3.TDH.other.chunk1$counts, {
+    c(min(chromStart), max(chromEnd))/1e3
+  }))+
+  facet_grid(sample.id ~ ., scales="free", labeller=function(var, val){
+    sub("McGill0", "", val)
+  })
+
+png("figure-timings-profiles.png", width=9, h=7, res=200, units="in")
+print(problemsPlot)
+dev.off()
+
+joint.times <- do.call(rbind, joint.time.list)
+joint.by.res <- split(joint.times, joint.times$bases.per.problem)
+best.times <- joint.by.res[[best.res.str]]
+best.times.df <-
+  rbind(best.times[, c("data", "seconds")],
+        data.frame(data=NA, seconds=sum(best.times$seconds), row.names="total"))
+
+single.times <- do.call(rbind, timing.list)
+ggplot()+
+  geom_point(aes(data, seconds), data=joint.times)
+resolution.times <- joint.times %>%
+  group_by(bases.per.problem) %>%
+  summarise(seconds=sum(seconds))
+
+single.times.df <-
+  rbind(single.times,
+        data.frame(seconds=sum(single.times$seconds), sample.id="total"))
+single.times.xt <- xtable(single.times.df)
+print(single.times.xt, include.rownames=FALSE, floating=FALSE,
+      file="table-timings-PeakSeg.tex")
+
+xt <- xtable(best.times.df)
+print(xt, file="table-timings.tex", floating=FALSE)
