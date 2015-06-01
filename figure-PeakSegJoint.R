0fc073e6493bb5a558087404feeae2cd8ec7e628
Toby Dylan Hocking <tdhock5@gmail.com>
2015-06-01 15:16:43 -0400

    Fig 2 panels for models, add Unconstrained model

--- figure-PeakSegJoint.R
+++ figure-PeakSegJoint.R
@@ -1,9 +1,13 @@
-works_with_R("3.1.3",
+works_with_R("3.2.0",
+             "tdhock/PeakSegDP@fe06a5b91d68c5d1ec471cb15c3ec3935dc2624d",
              "tdhock/PeakError@d9196abd9ba51ad1b8f165d49870039593b94732",
-             "tdhock/PeakSegJoint@547ca81ce92c38b7c7f78cd071efc5afa96cb289")
+             "tdhock/PeakSegJoint@86bee0a4620160e2d4b904e7819b5792280d51de")
 
 data(H3K36me3.TDH.other.chunk1)
-sample.ids <- sprintf("McGill%04d", c(22, 23, 19))
+sample.ids <- sprintf("McGill%04d", c(23, 22, 37))
+sample.y <- -seq_along(sample.ids)*100
+sample.h <- 40
+names(sample.y) <- sample.ids
 some.counts <-
   subset(H3K36me3.TDH.other.chunk1$counts,
          sample.id %in% sample.ids &
@@ -13,6 +17,8 @@ some.regions <-
   subset(H3K36me3.TDH.other.chunk1$regions,
          sample.id %in% sample.ids &
          chromStart < 43205000)
+is.first <- some.regions$chromEnd == min(some.regions$chromEnd)
+some.regions[is.first, "chromEnd"] <- 43168000
 
 library(ggplot2)
 ann.colors <-
@@ -21,15 +27,33 @@ ann.colors <-
     peakEnd="#ff4c4c",
     peaks="#a445ee")
 
+plevs <- c("sample 1", "sample 2", "sample 3",
+           "0 peaks", "1 peak", "2 peaks", "3 peaks")
+
+id2sample <- function(x){
+  sample.id <- factor(paste(x), sample.ids)
+  panel.str <- paste("sample", as.integer(sample.id))
+  factor(panel.str, plevs)
+}
+  
 sort.samples <- function(df){
   df$sample.id <- factor(df$sample.id, sample.ids)
+  panel.str <- paste("sample", as.integer(df$sample.id))
+  df$panel <- factor(panel.str, plevs)
   df
 }
 regions.plot <- 
   ggplot()+
     scale_y_continuous("aligned read coverage",
                        breaks=function(limits){
-                         c(0, floor(limits[2]))
+                         hi <- floor(limits[2])
+                         if(hi==10){
+                           c(0, hi)
+                         }else if(0 < hi){
+                           hi
+                         }else{
+                           -Inf
+                         }
                        })+
   xlab("position on chromosome (kilo bases = kb)")+
   geom_tallrect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
@@ -43,43 +67,74 @@ regions.plot <-
             color="grey50")+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "cm"))+
-  facet_grid(sample.id ~ ., scales="free", labeller=function(var, val){
-    paste("sample", seq_along(val))
-  })
+  facet_grid(panel ~ ., scales="free")
 
-fit <- PeakSegJointHeuristic(some.counts)
+profile.list <- ProfileList(some.counts)
+unconstrained.list <- list()
+for(sample.id in names(profile.list)){
+  pro <- profile.list[sample.id]
+  fit <- PeakSegJointSeveral(pro)
+  converted <- ConvertModelList(fit)
+  unconstrained.list[[sample.id]] <-
+    data.frame(converted$peaks, diff=diff(converted$loss$loss))
+}
+unconstrained.peaks.list <- list()
+for(peaks in seq_along(unconstrained.list)){
+  id.vec <- c("McGill0022", "McGill0023", "McGill0037")[1:peaks]
+  peak.df <- do.call(rbind, unconstrained.list[id.vec])
+  peak.df$peaks <- peaks
+  unconstrained.peaks.list[[paste(peaks)]] <- peak.df
+}
+fit <- PeakSegJointHeuristic(profile.list)
 converted <- ConvertModelList(fit)
-zoom.peak.list <- with(converted, split(peaks, peaks$peaks))
-zoom.peak.list[["0"]] <- Peaks()
-
-## Compute PeakError on this sequence of models.
-regions.by.sample <- split(some.regions, some.regions$sample.id, drop=TRUE)
-counts.by.sample <- split(some.counts, some.counts$sample.id, drop=TRUE)
+converted$peaks$diff <- NA
+peaks.by.model <- 
+  list(
+       Unconstrained=unconstrained.peaks.list,
+    PeakSegJoint=with(converted, split(peaks, peaks$peaks)))
 error.region.list <- list()
 error.peaks.list <- list()
 peak.label.list <- list()
-for(peaks.str in names(zoom.peak.list)){
-  several.samples <- zoom.peak.list[[peaks.str]]
-  peaks.by.sample <- split(several.samples, several.samples$sample.id)
-  peaks <- as.numeric(peaks.str)
-  for(sample.id in names(regions.by.sample)){
-    one.sample.peaks <- if(sample.id %in% names(peaks.by.sample)){
-      peaks.by.sample[[sample.id]]
-    }else{
-      Peaks()
-    }
-    max.count <- max(counts.by.sample[[sample.id]]$count)
-    y <- (peaks+1) * -0.15 * max.count
-    h <- -0.05 * max.count
-    peak.label.list[[paste(sample.id, peaks)]] <-
-      data.frame(sample.id, peaks, y)
-    one.sample.regions <- regions.by.sample[[sample.id]]
-    error <- PeakErrorChrom(one.sample.peaks, one.sample.regions)
-    error.region.list[[paste(sample.id, peaks)]] <-
-      data.frame(sample.id, peaks, error, y, h)
-    if(nrow(one.sample.peaks)){
-      error.peaks.list[[paste(sample.id, peaks)]] <-
-        data.frame(one.sample.peaks, y, h)
+for(model in names(peaks.by.model)){
+  zoom.peak.list <- peaks.by.model[[model]]
+  zoom.peak.list[["0"]] <- Peaks()
+
+  ## Compute PeakError on this sequence of models.
+  regions.by.sample <- split(some.regions, some.regions$sample.id, drop=TRUE)
+  counts.by.sample <- split(some.counts, some.counts$sample.id, drop=TRUE)
+  for(peaks.str in names(zoom.peak.list)){
+    several.samples <- zoom.peak.list[[peaks.str]]
+    peaks.by.sample <- split(several.samples, several.samples$sample.id)
+    peaks <- as.numeric(peaks.str)
+    for(sample.id in names(regions.by.sample)){
+      one.sample.peaks <- if(sample.id %in% names(peaks.by.sample)){
+        peaks.by.sample[[sample.id]]
+      }else{
+        Peaks()
+      }
+      max.count <- max(counts.by.sample[[sample.id]]$count)
+      y <- (peaks+1) * -0.15 * max.count
+      h <- -0.05 * max.count
+      peak.label.list[[paste(sample.id, peaks, model)]] <-
+        data.frame(sample.id, peaks, model, y)
+      one.sample.regions <- regions.by.sample[[sample.id]]
+      error <- PeakErrorChrom(one.sample.peaks, one.sample.regions)
+      panel.str <- sprintf("%d peak%s", peaks, ifelse(peaks==1, "", "s"))
+      panel <- factor(panel.str, plevs)
+      error.region.list[[paste(sample.id, peaks, model)]] <-
+        data.frame(sample.id, peaks, error, y, h,
+                   sample=id2sample(sample.id),
+                   panel,
+                   model,
+                   sample.y=sample.y[[sample.id]])
+      if(nrow(one.sample.peaks)){
+        error.peaks.list[[paste(sample.id, peaks, model)]] <-
+          data.frame(one.sample.peaks, y, h,
+                     sample=id2sample(sample.id),
+                     panel,
+                     model,
+                     sample.y=sample.y[[sample.id]])
+      }
     }
   }
 }
@@ -88,12 +143,26 @@ error.peaks <- do.call(rbind, error.peaks.list)
 error.regions <- do.call(rbind, error.region.list)
 peak.labels <- do.call(rbind, peak.label.list)
 
+first.regions <- subset(error.regions, chromStart==min(chromStart))
+
 err.plot <- 
 regions.plot+
+  geom_text(aes(chromStart/1e3, sample.y,
+                label=paste0(sample, " ")),
+            hjust=1,
+            size=3.5,
+            data=first.regions)+
+  geom_rect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
+                fill=annotation,
+                ymin=sample.y-sample.h, ymax=sample.y+sample.h),
+            color="grey",
+            alpha=0.5,
+            data=error.regions)+
   geom_rect(aes(xmin=chromStart/1e3, xmax=chromEnd/1e3,
                 linetype=status,
-                ymin=y-h, ymax=y+h),
+                ymin=sample.y-sample.h, ymax=sample.y+sample.h),
             fill=NA,
+            size=1,
             color="black",
             data=error.regions)+
   scale_linetype_manual("error type",
@@ -104,19 +173,21 @@ regions.plot+
                         values=c(correct=0,
                           "false negative"=3,
                           "false positive"=1))+
-  geom_text(aes(43140,
-                y, label=paste0(peaks, " peak",
-                     ifelse(peaks==1, "", "s"),
-                                " ")),
-            size=2.5,
-            hjust=0,
-            data=peak.labels)+
-  geom_segment(aes(chromStart/1e3, y,
-                   xend=chromEnd/1e3, yend=y),
-               size=1,
-               color="deepskyblue",
+  ## geom_text(aes(43140,
+  ##               y, label=paste0(peaks, " peak",
+  ##                    ifelse(peaks==1, "", "s"),
+  ##                               " ")),
+  ##           size=2.5,
+  ##           hjust=0,
+  ##           data=peak.labels)+
+  scale_size_manual(values=c(Unconstrained=3, PeakSegJoint=1.5))+
+  scale_color_manual(values=c(Unconstrained="royalblue",
+                       PeakSegJoint="deepskyblue"))+
+  geom_segment(aes(chromStart/1e3, sample.y,
+                   color=model, size=model,
+                   xend=chromEnd/1e3, yend=sample.y),
                data=data.frame(what="peaks", error.peaks))
 
-png("figure-PeakSegJoint.png", width=7, height=4, units="in", res=200)
+png("figure-PeakSegJoint.png", width=8, height=5, units="in", res=200)
 print(err.plot)
 dev.off()
